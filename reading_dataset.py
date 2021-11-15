955763ea27c1ddd20665052c06a3cf9f4a52e603
kamil.wolny <external.kamil.wolny@kuehne-nagel.com>
2021-11-15 22:33:40 +0100

    clean up

--- reading_dataset.py
+++ reading_dataset.py
@@ -1,4 +1,5 @@
 import os
+
 from PIL import Image
 
 cwd = os.getcwd()
@@ -6,7 +7,8 @@ cwd = os.getcwd()
 BASE_PATH = os.path.join(cwd, "dataset", "fruits_360_dataset", "fruits")
 
 TRAIN_PATH = os.path.join(BASE_PATH, "Training")
-TEST_PATH  = os.path.join(BASE_PATH, "Test")
+TEST_PATH = os.path.join(BASE_PATH, "Test")
+
 
 def delete_background(img):
     img = img.convert("RGBA")
@@ -21,15 +23,16 @@ def delete_background(img):
         else:
             newData.append(item)
     img.putdata(newData)
-  
+
     return img
-    
+
 
 def save_in_new_dir(img, dirname, filename):
     new_dir = dirname.replace("fruits_360_dataset", "without_background")
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
     img.save(os.path.join(new_dir, filename), "PNG")
+
 
 for path_ in [TEST_PATH, TRAIN_PATH]:
     for dirname, _, filenames in os.walk(path_):
@@ -38,4 +41,3 @@ for path_ in [TEST_PATH, TRAIN_PATH]:
             img = Image.open(image)
             img = delete_background(img)
             save_in_new_dir(img, dirname, filename)
-    
