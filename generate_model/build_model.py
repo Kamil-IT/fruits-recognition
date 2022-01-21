9efe5abe33f34cdc3cb01510b3963718d226801b
nielogicznie <254292@student.pwr.edu.pl>
2022-01-21 10:58:01 +0100

    added style

--- generate_model/build_model.py
+++ generate_model/build_model.py
@@ -2,8 +2,54 @@ import datetime
 
 import tensorflow as tf
 
-from generate_model.import_images import import_images
-from generate_model.properties import MODEL_PATH
+# from generate_model.import_images import import_images
+# from generate_model.properties import MODEL_PATH
+
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+# from generate_model.properties import TRAIN_DIR, VAL_DIR, TEST_DIR
+
+import os
+cwd = os.getcwd()
+
+
+TRAIN_DIR = os.path.join(cwd, 'dataset','without_background','fruits','Training')
+VAL_DIR = os.path.join(cwd, 'dataset','without_background','fruits','Validate')
+TEST_DIR = os.path.join(cwd, 'dataset','without_background','fruits','Test')
+
+MODEL_PATH = r"model"
+MODEL_H5_PATH = r"model/fruit.h5"
+
+
+def import_images():
+    training_datagen = ImageDataGenerator(rescale=1. / 255)
+    validation_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train = training_datagen.flow_from_directory(TRAIN_DIR,
+                                                 target_size=(100, 100),
+                                                 shuffle=True,
+                                                 batch_size=34,
+                                                 color_mode='rgb',
+                                                 class_mode="categorical")  # multi-class classification.
+
+    val = validation_datagen.flow_from_directory(VAL_DIR,
+                                                 target_size=(100, 100),
+                                                 shuffle=True,
+                                                 color_mode='rgb',
+                                                 class_mode="categorical")  # multi-class classification.
+
+    test = test_datagen.flow_from_directory(TEST_DIR,
+                                            target_size=(100, 100),
+                                            color_mode='rgb',
+                                            batch_size=49,
+                                            class_mode="categorical",  # multi-class classification.
+                                            shuffle=False)
+
+    print(train.class_indices)
+    print(val.class_indices)
+    print(test.class_indices)
+    return train, val, test
 
 
 def prepare_model_to_learn():
@@ -19,6 +65,9 @@ def prepare_model_to_learn():
         tf.keras.layers.MaxPooling2D(2, 2),
         # fourth convolution layer
         tf.keras.layers.Conv2D(128, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        # fifth convolution layer
+        tf.keras.layers.Conv2D(512, (3, 3), activation="relu"),
         tf.keras.layers.MaxPooling2D(2, 2),
         # flatten before feeding into Dense neural network.
         tf.keras.layers.Flatten(),
@@ -66,13 +115,13 @@ fruit_model = model.fit(train,
                         validation_steps=20,  # 20 x 34 (batch size) = 640 images
                         )
 
-# Save
-tf.keras.models.save_model(
-    model,
-    MODEL_PATH,
-    overwrite=True,
-    include_optimizer=True,
-    save_format="tf",
-    signatures=None
-)
-model.save(r"model/fruit.h5")
+# # Save
+# tf.keras.models.save_model(
+#     model,
+#     MODEL_PATH,
+#     overwrite=True,
+#     include_optimizer=True,
+#     save_format="tf",
+#     signatures=None
+# )
+# model.save(r"model/fruit.h5")
